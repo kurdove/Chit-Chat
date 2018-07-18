2d7a77829dcc810c8423fcbdc235f796a2e3846d
Evgheni Curdov <kurdov.e@gmail.com>
2018-07-18 08:06:51 -0700

    RoomList works as needed

--- src/App.js
+++ src/App.js
@@ -21,13 +21,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <aside>
+
+        <aside className="Side-bar">
           <RoomList firebase = {firebase} />
         </aside>
       </div>
