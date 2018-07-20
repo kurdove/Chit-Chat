dd3fd99c8648ae8ec5080829e9a3bcf8bea4fe74
Evgheni Curdov <kurdov.e@gmail.com>
2018-07-20 09:51:37 -0700

    New room in progress

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import '.././styles/RoomList.css';
 
 class RoomList extends Component{
   constructor(props){
     super(props);
-    this.state={rooms:[]};
+    this.state={
+      rooms:[],
+      newRoomName: ''
+    };
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.createRoom = this.createRoom.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -15,17 +21,42 @@ class RoomList extends Component{
      });
    }
 
+  createRoom(newRoomName) {
+    this.roomsRef.push({ name: newRoomName});
+    this.setState({ newRoomName: '' });
+ }
+
+   handleSubmit(e){
+     e.preventDefault();
+     this.createRoom();
+ }
+
+  handleChange(e) {
+    e.preventDefault();
+    // console.log('change')
+    this.setState({newRoomName: e.target.value});
+  }
+
    render() {
-
-
      return (
        <section>
+
          <h1> Room List </h1>
          {this.state.rooms.map( room =>
-             <li key={room.key}>
+            <ul key={room.key}>
+             <li>
                {room.name}
              </li>
+            </ul>
          )}
+
+         <section className="new-room-form">
+           <form onSubmit={this.handleSubmit}>
+             <span>Create New Room:<br/></span>
+             <input type="text" placeholder="New Room" onChange={this.handleChange} />
+             <input type="submit" value="CREATE"/>
+           </form>
+         </section>
 
        </section>
      );
